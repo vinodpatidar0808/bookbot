c4713bd4172911d228fdffe0314f19ba798b6e9a
vinodpatidar0808 <vinodpatidar0808@gmail.com>
2025-01-29 22:42:07 +0530

    add count character function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def count_characters(str):
+  count = {}
+  for char in str:
+    character_lower = char.lower()
+    if character_lower in count:
+      count[character_lower] = count[character_lower] + 1
+    else :
+      count[character_lower] = 1
+
+  return count
+
+
 def count_words(str):
   return len(str.split())
 
@@ -6,6 +18,7 @@ def main():
     file_contents = f.read()
     # print(file_contents)
     print(count_words(file_contents))
+    print(count_characters(file_contents))
 
 
 
