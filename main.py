9a52a8370565853c1db45072f161a7a2b59fcb6c
vinodpatidar0808 <vinodpatidar0808@gmail.com>
2025-01-29 22:48:19 +0530

    add generate report function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def generate_report(words, character_count):
+  print("--- Begin report of books/frankenstein.txt ---")
+  print(f"{words} words found in the document")
+  for key,val in character_count.items():
+    if key.isalpha():
+      print(f"The '{key}' character was found {val} times")
+  print("--- End report ---")
+
 def count_characters(str):
   count = {}
   for char in str:
@@ -17,8 +25,9 @@ def main():
   with open("./books/frankenstein.txt") as f: 
     file_contents = f.read()
     # print(file_contents)
-    print(count_words(file_contents))
-    print(count_characters(file_contents))
+    # print(count_words(file_contents))
+    # print(count_characters(file_contents))
+    generate_report(count_words(file_contents), count_characters(file_contents))
 
 
 
